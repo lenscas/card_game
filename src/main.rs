9750e4c777d0d55e924f3acd15bb94decc34b706
lenscas <lenscas@gmail.com>
2020-02-09 12:56:53 +0100

    make it work

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 use dotenv::{dotenv, var};
-use futures::future::*;
 use sqlx::{query, PgPool};
 use warp::Filter;
 
+async fn handle_from_db(
+    id: i32,
+    pool: PgPool,
+) -> Result<impl warp::Reply, warp::reject::Rejection> {
+    let mut con = pool.acquire().await.unwrap();
+    match query!("SELECT username FROM users WHERE id = $1", id)
+        .fetch_one(&mut con)
+        .await
+    {
+        Ok(v) => Ok(v.username),
+        Err(_) => Err(warp::reject::not_found()),
+    }
+}
 #[tokio::main]
 async fn main() {
     dotenv().unwrap();
@@ -12,32 +24,13 @@ async fn main() {
     let pool = PgPool::new(&db_url)
         .await
         .expect("Couldn't connect to database");
-    /*
-    let v = query!("SELECT id,username,password FROM users WHERE id = $1", 1)
-        .fetch_all(&mut pool)
-        .await
-        .unwrap();
-    v.iter().for_each(|rec| {
-        println!(
-            "id : {:0}, username : {:1}, password : {:2}",
-            rec.id, rec.username, rec.password
-        )
-    });*/
-    let x = || pool.clone();
     let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
 
-    let from_db = warp::path!("hello" / i32).map(|id| {
-        let bla = id;
-        async {
-            let mut con = x().try_acquire().unwrap();
-            query!("SELECT username FROM users WHERE id = $1", 1)
-                .fetch_one(&mut con)
-                .map(|v| match v {
-                    Ok(v) => Ok(v.username),
-                    Err(_) => Err(warp::reject::not_found()),
-                })
-        }
-    });
-    //.map(|name: String| format!("Hello from db {}", name));
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+    let from_db = warp::path!("hello" / i32)
+        .and(warp::any().map(move || pool.clone()))
+        .and_then(handle_from_db);
+
+    warp::serve(warp::get().and(from_db).or(hello))
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
