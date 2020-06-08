117b48343363c87e36e7961e750c4f4b148727b0
lenscas <lenscas@gmail.com>
2020-06-08 14:07:16 +0200

    use images from server

--- src/client.rs
+++ src/client.rs
@@ -5,17 +5,26 @@ use crate::{
 use card_game_shared::LoginData;
 use card_game_shared::ReturnBattle;
 use card_game_shared::TakeAction;
+use quicksilver::{graphics::Image, Graphics};
 use silver_surf::{call, Config, Method};
+use std::collections::HashMap;
+
+pub struct ReturnBattleWithImages {
+    pub(crate) images: Vec<Image>,
+    pub(crate) battle: ReturnBattle,
+}
 
 pub struct Client {
     base_url: String,
     authorization_code: Option<String>,
+    cached_images: HashMap<String, Image>,
 }
 impl Client {
     pub fn new(base_url: String) -> Client {
         Client {
             base_url,
             authorization_code: None,
+            cached_images: HashMap::new(),
         }
     }
     fn set_url(&self, part: &str) -> String {
@@ -46,7 +55,30 @@ impl Client {
         self.authorization_code = Some(v.token);
         Ok(())
     }
-    pub(crate) async fn new_battle(&mut self) -> Result<ReturnBattle> {
+    async fn load_image(&mut self, path: String, gfx: &Graphics) -> Result<Image> {
+        let headers = self.set_headers();
+        let url = self.set_url(&(String::from("assets/") + &path));
+
+        let entry = self.cached_images.entry(path.clone());
+        Ok(match entry {
+            std::collections::hash_map::Entry::Occupied(v) => v.get().clone(),
+            std::collections::hash_map::Entry::Vacant(x) => {
+                let res = call(Config::<()> {
+                    url,
+                    method: Method::Get,
+                    body: None,
+                    headers,
+                })?
+                .bytes()
+                .await?;
+                let image = Image::from_encoded_bytes(gfx, &res)?;
+                x.insert(image.clone());
+                image
+            }
+        })
+    }
+
+    pub(crate) async fn new_battle(&mut self, gfx: &Graphics) -> Result<ReturnBattleWithImages> {
         let res = call(Config::<()> {
             url: self.set_url("battle"),
             method: Method::Post,
@@ -55,17 +87,24 @@ impl Client {
         })?
         .json::<CustomResult<ReturnBattle>>()
         .await;
-        res?.into_dyn_res()
-        /*
-        let res = match res {
-            Ok(x) => x,
-            Err(x) => return Err(x),
-        };
-        Ok(res)
-        let res = Result::<_>::from(res)?;
-        Ok(res)*/
+        let res = res?.into_dyn_res()?;
+        let mut cards = Vec::new();
+        for id in &res.hand {
+            cards.push(
+                self.load_image(String::from("cards/") + &id + ".png", gfx)
+                    .await?,
+            );
+        }
+        Ok(ReturnBattleWithImages {
+            battle: res,
+            images: cards,
+        })
     }
-    pub(crate) async fn do_turn(&self, card: usize) -> Result<ReturnBattle> {
+    pub(crate) async fn do_turn(
+        &mut self,
+        card: usize,
+        gfx: &Graphics,
+    ) -> Result<ReturnBattleWithImages> {
         let res = call(Config {
             url: self.set_url("battle/"),
             method: Method::Put,
@@ -75,6 +114,17 @@ impl Client {
         .json::<CustomResult<ReturnBattle>>()
         .await;
         let res = dbg!(res);
-        res?.into_dyn_res()
+        let res = res?.into_dyn_res()?;
+        let mut cards = Vec::new();
+        for id in &res.hand {
+            cards.push(
+                self.load_image(String::from("cards/") + &id + ".png", gfx)
+                    .await?,
+            );
+        }
+        Ok(ReturnBattleWithImages {
+            battle: res,
+            images: cards,
+        })
     }
 }
--- src/screens/battle.rs
+++ src/screens/battle.rs
@@ -1,7 +1,7 @@
 use super::Screen;
 use async_trait::async_trait;
 use quicksilver::geom::{Circle, Rectangle, Shape, Vector};
-use quicksilver::graphics::{Color, FontRenderer, VectorFont};
+use quicksilver::graphics::{Color, FontRenderer, Image, VectorFont};
 use std::f64::consts::PI;
 
 use crate::Wrapper;
@@ -23,12 +23,10 @@ fn has_rune<'a>(
 pub struct Battle {
     outer_points: Vec<Circle>,
     inner_points: Vec<Circle>,
-    hand: Vec<(String, Rectangle)>,
+    hand: Vec<(Image, Rectangle)>,
     player_runes: Vec<String>,
     enemy_runes: Vec<String>,
     rotation: f64,
-    return_is_down: bool,
-    card_font: FontRenderer,
     stat_font: FontRenderer,
     clicked: bool,
     enemy_hp: String,
@@ -36,7 +34,6 @@ pub struct Battle {
     player_hp: String,
     enemy_mana: String,
     player_mana: String,
-    card_font_size: f32,
     hexa_runes: Vec<String>,
 }
 
@@ -57,12 +54,14 @@ fn calc_points(
         .collect()
 }
 
-fn get_location_of_cards(cards: Vec<String>, resolution: Vector) -> Vec<(String, Rectangle)> {
+fn get_location_of_cards(cards: Vec<Image>, resolution: Vector) -> Vec<(Image, Rectangle)> {
     cards
         .into_iter()
         .enumerate()
         .map(|(key, card)| {
-            let rec_size = Vector::new(0.1375 * resolution.x, 0.283_333_33 * resolution.y);
+            //original = 794 X 1123
+            //0.794 X 1.123
+            let rec_size = Vector::new(0.176_758_68 * resolution.y, 0.25 * resolution.y);
             let rec_location = Vector::new(
                 0.00625 * resolution.x,
                 (0.008_333_334 + (0.046_666_667 * key as f32)) * resolution.y,
@@ -89,12 +88,12 @@ impl Battle {
                 y + f64::from(0.500_833_33 * v.y),
             )
         });
-        let current = wrapper.client.new_battle().await?;
+        let current = wrapper.client.new_battle(&wrapper.gfx).await?;
+        let (current, hand) = (current.battle, current.images);
         let resolution = v;
-        let hand = get_location_of_cards(current.hand, resolution);
+        let hand = get_location_of_cards(hand, resolution);
 
         let font = VectorFont::load("font.ttf").await?;
-        let font_size = 0.023_333_333 * resolution.y;
 
         Ok(Battle {
             player_mana: current.mana.to_string(),
@@ -103,20 +102,17 @@ impl Battle {
             inner_points,
             rotation: 0.0,
             hand,
-            return_is_down: false,
             clicked: false,
             enemy_hand_size: format!("S: {}", current.enemy_hand_size),
             enemy_hp: format!("HP: {}", current.enemy_hp),
             player_hp: format!("HP: {}", current.player_hp),
             enemy_runes: current.enemy_small_runes,
             player_runes: current.small_runes,
-            card_font: font.to_renderer(&wrapper.gfx, font_size)?,
             stat_font: font.to_renderer(&wrapper.gfx, 25.0)?,
-            card_font_size: font_size,
             hexa_runes: current.hexa_runes,
         })
     }
-    async fn play_card(&mut self, wrapper: &Wrapper) -> crate::Result<()> {
+    async fn play_card(&mut self, wrapper: &mut Wrapper) -> crate::Result<()> {
         let cursor_pos = wrapper.get_cursor_loc();
         let chosen = self
             .hand
@@ -127,8 +123,9 @@ impl Battle {
             .find(|(_, card)| card.contains(cursor_pos))
             .map(|(k, _)| k);
         if let Some(chosen) = chosen {
-            let battle = wrapper.client.do_turn(chosen).await?;
-            self.hand = get_location_of_cards(battle.hand, wrapper.window.size());
+            let battle = wrapper.client.do_turn(chosen, &wrapper.gfx).await?;
+            let (battle, hand) = (battle.battle, battle.images);
+            self.hand = get_location_of_cards(hand, wrapper.window.size());
             self.enemy_hand_size = format!("S: {}", battle.enemy_hand_size);
             self.enemy_hp = format!("HP: {}", battle.enemy_hp);
             self.player_hp = format!("HP: {}", battle.player_hp);
@@ -146,7 +143,8 @@ impl Battle {
 impl Screen for Battle {
     async fn draw(&mut self, wrapper: &mut crate::Wrapper) -> crate::Result<()> {
         let resolution = wrapper.window.size();
-        wrapper.gfx.clear(Color::BLACK);
+        wrapper.gfx.clear(Color::from_hex("#031234"));
+
         self.outer_points
             .iter()
             .enumerate()
@@ -191,17 +189,8 @@ impl Screen for Battle {
             .gfx
             .stroke_path(&[(0., 0.).into(), resolution], Color::BLUE);
 
-        for (card, rectangle) in self.hand.iter_mut() {
-            let card = card;
-            let font_location = Vector::new(
-                (0.0025 * resolution.x) + rectangle.pos.x,
-                (0.0025 * resolution.x) + self.card_font_size + rectangle.pos.y,
-            );
-            let rec = Rectangle::new(rectangle.pos, rectangle.size);
-            wrapper.gfx.fill_rect(&rec, Color::WHITE);
-            wrapper.gfx.stroke_rect(&rec, Color::RED);
-            self.card_font
-                .draw(&mut wrapper.gfx, card, Color::BLACK, font_location)?;
+        for (card, rectangle) in self.hand.iter() {
+            wrapper.gfx.draw_image(card, *rectangle);
         }
         let renderer = &mut self.stat_font;
         let offset = wrapper.get_pos_vector(0.02, 0.95);
@@ -237,23 +226,8 @@ impl Screen for Battle {
         wrapper: &mut Wrapper,
         event: &quicksilver::input::Event,
     ) -> crate::Result<Option<Box<dyn Screen>>> {
-        use quicksilver::input::{Event::*, Key, MouseButton};
+        use quicksilver::input::{Event::*, MouseButton};
         match event {
-            KeyboardInput(x) => {
-                if x.key() == Key::Return {
-                    if x.is_down() && self.return_is_down {
-                        return Ok(None);
-                    } else if x.is_down() && !self.return_is_down {
-                        self.hand = get_location_of_cards(
-                            wrapper.client.do_turn(0).await?.hand,
-                            wrapper.window.size(),
-                        );
-                        self.return_is_down = true;
-                    } else if !x.is_down() {
-                        self.return_is_down = false;
-                    }
-                }
-            }
             PointerInput(x) if x.button() == MouseButton::Left => {
                 if x.is_down() {
                     if !self.clicked {
